06aa368bb36cefdeddc8a9c8232d02a566511aaf
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2023-06-04 15:01:47 +0700

    feat: endpoint-search:nameBrand

--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Req  } from '@nestjs/common';
+import { Controller, Post, Body, Req, Get, Query  } from '@nestjs/common';
 import { Request } from 'express';
 import { VehicleService } from './vehicle.service';
 import { CreateVehicleDto } from './dto/createVehicle.dto';
@@ -8,6 +8,21 @@ export class VehicleController {
     constructor(
         private readonly vehicleService: VehicleService,
     ) {}
+
+    @Get()
+    async findAll(
+        @Query('page') page: number = 1,
+        @Query('limit') limit: number = 3,
+    ) {
+        return this.vehicleService.findAll(page, limit);
+    }
+
+    @Get('search')
+    async findAllBrands(
+    @Query('brand') brand: string,
+    ) {
+        return this.vehicleService.findOne(brand);
+    }
 
     @Post('create')
     async createVehicle(@Body() createVehicleDto: CreateVehicleDto, @Req() req: Request) {
--- src/vehicle/vehicle.service.ts
+++ src/vehicle/vehicle.service.ts
@@ -1,18 +1,17 @@
-import { Injectable, UnauthorizedException} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { vehicle_brands } from '../vehicle-brands/entity/vehicle-brands.entity';
 import { vehicle_types } from '../vehicle-types/entity/vehicle-types.entity';
 import { vehicle_models } from '../vehicle-models/entity/vehicle-models.entity';
 import { vehicle_years } from '../vehicle-years/entity/vehicle-years.entity';
 import { pricelist } from '../pricelists/entity/pricelists.entity';
-import { Repository, Equal  } from 'typeorm';
+import { Repository, Equal } from 'typeorm';
 import { CreateVehicleDto } from './dto/createVehicle.dto';
 import { Request } from 'express';
 
 @Injectable()
 export class VehicleService {
-    constructor(
-        
+  constructor(
     @InjectRepository(vehicle_brands)
     private vehicleBrandsRepository: Repository<vehicle_brands>,
 
@@ -27,63 +26,118 @@ export class VehicleService {
 
     @InjectRepository(pricelist)
     private pricelistRepository: Repository<pricelist>,
-    ) {}
+  ) {}
 
-    async create(createVehicleDto: CreateVehicleDto, req: Request): Promise<any> {
-        const { brandName, typeName, modelName, year, price } = createVehicleDto;
-        if (req['user'].role !== true) {
-            throw new UnauthorizedException('Only admin can create pricelist');
-          }
-          
-        let vehicleBrand = await this.vehicleBrandsRepository.findOne({
-          where: { name: brandName },
-        });
-        if (!vehicleBrand) {
-          vehicleBrand = new vehicle_brands();
-          vehicleBrand.name = brandName;
-          vehicleBrand = await this.vehicleBrandsRepository.save(vehicleBrand);
-        }
-    
-        let vehicleType = await this.vehicleTypesRepository.findOne({
-          where: { name: typeName, id_brand: Equal(vehicleBrand.id_brand) },
-        });
-        if (!vehicleType) {
-          vehicleType = new vehicle_types();
-          vehicleType.name = typeName;
-          vehicleType.id_brand = vehicleBrand;
-          vehicleType = await this.vehicleTypesRepository.save(vehicleType);
-        }
-    
-        let vehicleModel = await this.vehicleModelsRepository.findOne({
-          where: { name: modelName, id_type: Equal(vehicleType.id_type) },
-        });
-        if (!vehicleModel) {
-          vehicleModel = new vehicle_models();
-          vehicleModel.name = modelName;
-          vehicleModel.id_type = vehicleType;
-          vehicleModel = await this.vehicleModelsRepository.save(vehicleModel);
-        }
-    
-        let vehicleYear = await this.vehicleYearsRepository.findOne({
-          where: { year },
-        });
-        if (!vehicleYear) {
-          vehicleYear = new vehicle_years();
-          vehicleYear.year = year;
-          vehicleYear = await this.vehicleYearsRepository.save(vehicleYear);
-        }
-    
-        const newPricelist = new pricelist();
-        newPricelist.price = price;
-        newPricelist.id_year = vehicleYear;
-        newPricelist.id_model = vehicleModel;
-    
-        return this.pricelistRepository.save(newPricelist);
+  async create(createVehicleDto: CreateVehicleDto, req: Request): Promise<any> {
+    const { brandName, typeName, modelName, year, price } = createVehicleDto;
+    if (req['user'].role !== true) {
+      throw new UnauthorizedException('Only admin can create pricelist');
+    }
+
+    let vehicleBrand = await this.vehicleBrandsRepository.findOne({
+      where: { name: brandName },
+    });
+    if (!vehicleBrand) {
+      vehicleBrand = new vehicle_brands();
+      vehicleBrand.name = brandName;
+      vehicleBrand = await this.vehicleBrandsRepository.save(vehicleBrand);
+    }
+
+    let vehicleType = await this.vehicleTypesRepository.findOne({
+      where: { name: typeName, id_brand: Equal(vehicleBrand.id_brand) },
+    });
+    if (!vehicleType) {
+      vehicleType = new vehicle_types();
+      vehicleType.name = typeName;
+      vehicleType.id_brand = vehicleBrand;
+      vehicleType = await this.vehicleTypesRepository.save(vehicleType);
+    }
+
+    let vehicleModel = await this.vehicleModelsRepository.findOne({
+      where: { name: modelName, id_type: Equal(vehicleType.id_type) },
+    });
+    if (!vehicleModel) {
+      vehicleModel = new vehicle_models();
+      vehicleModel.name = modelName;
+      vehicleModel.id_type = vehicleType;
+      vehicleModel = await this.vehicleModelsRepository.save(vehicleModel);
+    }
+
+    let vehicleYear = await this.vehicleYearsRepository.findOne({
+      where: { year },
+    });
+    if (!vehicleYear) {
+      vehicleYear = new vehicle_years();
+      vehicleYear.year = year;
+      vehicleYear = await this.vehicleYearsRepository.save(vehicleYear);
+    }
+
+    const newPricelist = new pricelist();
+    newPricelist.price = price;
+    newPricelist.id_year = vehicleYear;
+    newPricelist.id_model = vehicleModel;
+
+    return this.pricelistRepository.save(newPricelist);
+  }
+
+  async findAll(page: number, limit: number): Promise<any> {
+    const skip = (page - 1) * limit;
+
+    const [pricelists, total] = await this.pricelistRepository.findAndCount({
+      relations: [
+        'id_model',
+        'id_model.id_type',
+        'id_model.id_type.id_brand',
+        'id_year',
+      ],
+      take: limit,
+      skip: skip,
+    });
+
+    const formattedPricelists = pricelists.map((pricelist) => ({
+        year: pricelist.id_year.year,
+        brandName: pricelist.id_model.id_type.id_brand.name,
+        modelName: pricelist.id_model.name,
+        typeName: pricelist.id_model.id_type.name,
+        price: pricelist.price,
+    }));
+
+    const totalPages = Math.ceil(total / limit);
+    const baseUrl = '/vehicle';
+    const previous = page > 1 ? page - 1 : null;
+    const next = page < totalPages ? page + 1 : null;
+    const nextPageUrl = next ? `${baseUrl}?page=${next}` : null;
+    const previousPageUrl = previous ? `${baseUrl}?page=${previous}` : null;
+
+    return {
+      page,
+      limit,
+      total,
+      previous: previousPageUrl,
+      next: nextPageUrl,
+      data: formattedPricelists,
+    };
+  }
+
+  async findOne(nameBrand: string): Promise<any> {
+    const brand = await this.vehicleBrandsRepository.findOne({ where: { name: nameBrand } });
+    if (!brand) {
+        return { message: 'Brand not found' };
       }
 
-      async findAll(): Promise<any> {
-        const pricelists = await this.pricelistRepository.find({
-            relations: ['id_model', 'id_model.id_type', 'id_model.id_type.id_brand', 'id_year'],
-          });  
-    }
+      const pricelists = await this.pricelistRepository.find({
+        relations: ['id_model', 'id_model.id_type', 'id_model.id_type.id_brand', 'id_year'],
+      });
+
+      const filteredPricelists = pricelists.filter(pricelist => pricelist.id_model.id_type.id_brand.name === nameBrand);
+
+      return filteredPricelists.map(pricelist => ({
+        year: pricelist.id_year.year,
+        brandName: pricelist.id_model.id_type.id_brand.name,
+        modelName: pricelist.id_model.name,
+        typeName: pricelist.id_model.id_type.name,
+        price: pricelist.price,
+      }));
+
+  }
 }
