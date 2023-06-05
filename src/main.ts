2d44798d33d6f1085ce6481150675f1892d9f27b
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2023-06-05 11:12:28 +0700

    feat: CORS

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  // Konfigurasi CORS
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
